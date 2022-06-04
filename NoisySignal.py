bc7025e172558a5baa3b174fd60b857553865b60
Alejandro G-Quintero <42096469+alejogq@users.noreply.github.com>
2022-06-04 15:57:21 -0400

    New Data from Rocket launch and new graphs
    New Data from Rocket launch and new graphs

--- NoisySignal.py
+++ NoisySignal.py
@@ -9,21 +9,26 @@ Created on Sat May 21 15:23:16 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
+
+#### Noise description
 # noise = np.random.normal(0, 1, 100)
-
-
-
 # 0 is the mean of the normal distribution you are choosing from
 # 1 is the standard deviation of the normal distribution
 # 100 is the number of elements you get in array noise
 
+col_list = ["Time","Altitude", "Velocity", "Acceleration"]
 
-col_list = ["Time", "Altitude", "Velocity", "Acceleration"]
-
-df = pd.read_csv('BV_timealtvelaccel.csv', comment='#', names=col_list)
+df = pd.read_csv('Data_CSV.csv', comment='#', names=col_list)
 # plt.plot(df)
 #Comment Alejandro
+
+timeX=df["Time"]
+
+df["TimeAlt"]=timeX
+df["TimeAcc"]=timeX
 
 IndMaxVal = df['Altitude'].idxmax()
 MaxVal = df["Altitude"].max()
@@ -33,30 +38,23 @@ NumEl = len(df['Altitude'])
 Adding noise to the signal in each dataframe
 """
 #noise for Altitude
-AltScale = 2
-noiseAlt = np.random.normal(0, 1, NumEl) * AltScale
+noiseAlt = np.random.normal(0, 0.389, NumEl) + 0.82 #Normal noise + bias
 
-#noise for Time
-TimeScale = 0.00001
-noiseTime = np.random.normal(0, 1, NumEl) * TimeScale
+#noise for Time Altitude
+noiseTimeAlt = np.random.normal(0.041, 0.006, NumEl)
+
+#noise for Time Acceleration
+noiseTimeAcc = np.random.normal(0.0108, 0.0018, NumEl)
 
 #noise for Acceleration
-AccScale = 5
-noiseAcc = np.random.normal(0, 1, NumEl) * AccScale
-
+noiseAcc = np.random.normal(0, 0.0148, NumEl) - 0.374 #normal noise + bias
 
 df['Altitude']     = noiseAlt + df['Altitude']
-df['Time']         = noiseTime + df['Time']
+df['TimeAlt']      = noiseTimeAlt + df['TimeAlt']
+
 df['Acceleration'] = noiseAlt + df['Acceleration']
+df['TimeAcc']      = noiseTimeAcc + df['TimeAcc']
 
-
-# plt.plot(noise)
-
-# NSignal = df["Altitude"] + noiseAlt
-
-
-# plt.plot(NSignal)
-# plt.hist(NSignal)
 
 N = 10 # number of samples the rocket will read at a time
 
@@ -64,39 +62,57 @@ newH = [0]
 newA = [0]
 vEstimation = [0]
 
-
 for i in range(0, (len(df["Time"]) - 20), 1):
 
     # simulate how the rocket will read the data the last N samples
-    rocketTime = df["Time"][i:i + N].to_numpy()
+    rocketTimeAlt = df["TimeAlt"][i:i + N].to_numpy()
+    rocketTimeAcc = df["TimeAcc"][i:i + N].to_numpy()
+    
     rocketAlt = df["Altitude"][i:i + N].to_numpy()
     rocketAccel = -1 * df["Acceleration"][i:i + (N // 2)].to_numpy()
 
     # change in altitude component
-    num = rocketTime.sum() * rocketAlt.sum() - N * (np.multiply(rocketTime, rocketAlt)).sum()
-    denom = rocketTime.sum() ** 2 - N * np.square(rocketTime).sum()
+    num = rocketTimeAlt.sum() * rocketAlt.sum() - N * (np.multiply(rocketTimeAlt, rocketAlt)).sum()
+    denom = rocketTimeAlt.sum() ** 2 - N * np.square(rocketTimeAlt).sum()
     h = num / denom
 
     # change in acceleration component
     for t in range(0, N // 2 - 1):
         a = (1 / 2) * ((rocketAccel[t] + rocketAccel[t + 1]) *
-                       (rocketTime[t + 1] - rocketTime[t]))
+                       (rocketTimeAcc[t + 1] - rocketTimeAcc[t]))
 
     #add components to get current velocity
     v = h + a
-
   
     newH.append(h)
     newA.append(a)
     vEstimation.append(v)
 
+# plt.plot(df["Velocity"], label="real velocity")
+# plt.plot(vEstimation, label="estimated velocity")
+
+# plt.legend()
+# plt.show()
+
+### graph in different pltots
+# fig = px.line(df, y="Velocity", x="TimeAlt")
+# fig = px.line(df, y="Acceleration", x="TimeAcc")
+# fig.write_html("Graph.html")
+# fig.show()
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df["TimeAlt"], y=df["Velocity"],
+                    mode='lines',
+                    name='Velocity'))
+
+fig.add_trace(go.Scatter(x=df["TimeAcc"], y=df["Acceleration"],
+                    mode='lines',
+                    name='lines'))
+fig.add_trace(go.Scatter(x=df["TimeAlt"], y=df["Altitude"],
+                    mode='lines',
+                    name='Acceleration'))
 
 
-plt.plot(df["Velocity"], label="real velocity")
-plt.plot(vEstimation, label="estimated velocity")
+fig.write_html("Graph.html")
+fig.show()
 
-# plt.plot(newH, label=" a")
-# plt.plot(newA, label="h")
-
-plt.legend()
-plt.show()
